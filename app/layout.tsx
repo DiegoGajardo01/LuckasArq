043c3415e5c71fba43fec9b5a89a089fe70c2f63
DiegoGajardo01 <diegogajardo1@gmail.com>
2025-05-15 00:22:36 -0400

    bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./ui/globals.css";
-import Header from './components/Header';
 import Footer from './components/Footer';
 
 const geistSans = Geist({
@@ -32,7 +31,6 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Header/>
         {children}
         <Footer/>
       </body>
